e04bb815871f88aa76dcf77c9f5c6ad3bf4c13a2
Kiran Challa <kichalla@microsoft.com>
2016-07-11 17:17:06 -0700

    Temp change - updated to use pooled buffers

--- src/Microsoft.AspNetCore.Mvc.Core/ContentResult.cs
+++ src/Microsoft.AspNetCore.Mvc.Core/ContentResult.cs
@@ -2,10 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Buffers;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc.Internal;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -19,6 +18,9 @@ namespace Microsoft.AspNetCore.Mvc
         {
             Encoding = Encoding.UTF8
         }.ToString();
+
+        private const int DefaultCharBufferSize = 4 * 1024;
+        private const int DefaultByteBufferSize = 8 * 1024;
 
         /// <summary>
         /// Gets or set the content representing the body of the response.
@@ -35,7 +37,7 @@ namespace Microsoft.AspNetCore.Mvc
         /// </summary>
         public int? StatusCode { get; set; }
 
-        public override Task ExecuteResultAsync(ActionContext context)
+        public override async Task ExecuteResultAsync(ActionContext context)
         {
             if (context == null)
             {
@@ -67,16 +69,59 @@ namespace Microsoft.AspNetCore.Mvc
 
             if (Content != null)
             {
-                var data = resolvedContentTypeEncoding.GetBytes(Content);
 
-                response.ContentLength = data.Length;
+                // Calculate the byte length to set the content length header
+                var charBuffer = ArrayPool<char>.Shared.Rent(DefaultCharBufferSize);
+                int sourceIndex = 0;
+                long byteCount = 0;
+                var encoder = resolvedContentTypeEncoding.GetEncoder();
 
-                return response.Body.WriteAsync(data, 0, data.Length);
+                while (sourceIndex < Content.Length)
+                {
+                    var countOfCharsToCopy = Math.Min((Content.Length - sourceIndex), charBuffer.Length);
+
+                    Content.CopyTo(sourceIndex, charBuffer, 0, countOfCharsToCopy);
+
+                    byteCount += encoder.GetByteCount(charBuffer, 0, countOfCharsToCopy, flush: false);
+
+                    sourceIndex += countOfCharsToCopy;
+                }
+
+                response.ContentLength = byteCount;
+
+                // write the content to stream
+                var byteBuffer = ArrayPool<byte>.Shared.Rent(DefaultByteBufferSize);
+
+                try
+                {
+                    sourceIndex = 0;
+                    while (sourceIndex < Content.Length)
+                    {
+                        var numOfCharsToCopy = Math.Min((Content.Length - sourceIndex), charBuffer.Length);
+
+                        Content.CopyTo(
+                            sourceIndex,
+                            charBuffer,
+                            0,
+                            numOfCharsToCopy);
+
+                        sourceIndex += numOfCharsToCopy;
+
+                        var bytesWritten = encoder.GetBytes(charBuffer, 0, numOfCharsToCopy, byteBuffer, 0, flush: false);
+
+                        await response.Body.WriteAsync(byteBuffer, 0, bytesWritten);
+                    }
+                }
+                finally
+                {
+                    // free the buffers
+                    ArrayPool<char>.Shared.Return(charBuffer);
+                    ArrayPool<byte>.Shared.Return(byteBuffer);
+                }
             }
             else
             {
                 response.ContentLength = 0;
-                return TaskCache.CompletedTask;
             }
         }
     }
